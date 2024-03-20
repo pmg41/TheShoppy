116e7368eb7117d9d57c19430888f047f8a665c3
pragnesh30gajera <pragnesh30gajera@gmail.,com>
2024-03-19 21:52:17 -0400

    - Issue resolved

--- app/src/main/java/com/pmg/theshoppy/MainActivity.java
+++ app/src/main/java/com/pmg/theshoppy/MainActivity.java
@@ -25,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     String[] provinces = {"Alberta", "British Columbia", "Manitoba", "New Brunswick", "Newfoundland and Labrador", "Nova Scotia", "Ontario", "Prince Edward Island", "Quebec", "Saskatchewan"};
     String[] computerBrands = {"Dell", "HP", "Lenovo"};
 
+    String selectedPeripheral = "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,91 +52,112 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private boolean validateDataAndCalculateInvoice() {
-        String name = binding.etCustomerName.getText().toString();
-        String province = binding.actProvince.getText().toString();
-        String brand = binding.spinnerBrand.getSelectedItem().toString();
-        boolean ssd = binding.cbSsd.isChecked();
-        boolean printer = binding.cbPrinter.isChecked();
-        int peripheralsId = binding.rgPeripherals.getCheckedRadioButtonId();
-        RadioButton peripherals = findViewById(peripheralsId);
-        String selectedPeripherals = peripherals.getText().toString();
+        try {
+            String name = (binding.etCustomerName.getText() == null) ? "" : binding.etCustomerName.getText().toString();
+            String province = (binding.actProvince.getText() == null) ? "" : binding.actProvince.getText().toString();
+            String brand = selectedBrand;
+            if (name.isEmpty()) {
+                Toast.makeText(this, "Please, enter name first!!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+            if (brand.isEmpty()) {
+                Toast.makeText(this, "Please, select brand first!!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+            if (province.isEmpty()) {
+                Toast.makeText(this, "Please,enter province first!!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+            if (selectedComputerType.isEmpty()) {
+                Toast.makeText(this, "Please select Computer type first!!", Toast.LENGTH_SHORT).show();
+                return false;
+            }
 
-        if (selectedComputerType.isEmpty()) {
-            Toast.makeText(this, "Please select Computer type first!!", Toast.LENGTH_SHORT).show();
-            return false;
-        }
-        if (brand.isEmpty()) {
-            Toast.makeText(this, "Please select Brand type first!!", Toast.LENGTH_SHORT).show();
-            return false;
-        }
+            boolean ssd = binding.cbSsd.isChecked();
+            boolean printer = binding.cbPrinter.isChecked();
+            int peripheralsId = binding.rgPeripherals.getCheckedRadioButtonId();
 
-        double cost = 0;
-        if (selectedComputerType.equals("Desktop")) {
-            if (brand.equals("Dell")) {
-                cost = 475;
-            } else if (brand.equals("HP")) {
-                cost = 400;
-            } else if (brand.equals("Lenovo")) {
-                cost = 450;
+            if (peripheralsId == -1) {
+                Toast.makeText(this, "Please select Peripherals related brand first!!", Toast.LENGTH_SHORT).show();
+                return false;
             }
-            if (ssd) {
-                cost += 60;
-            }
-            if (printer) {
-                cost += 100;
-            }
-            if (selectedPeripherals.equals("Webcam")) {
-                cost += 95;
-            } else if (selectedPeripherals.equals("External Hard Drive")) {
-                cost += 64;
-            }
-        } else if (selectedComputerType.equals("Laptop")) {
-            switch (brand) {
-                case "Dell":
-                    cost = 1249;
-                    break;
-                case "HP":
-                    cost = 1150;
-                    break;
-                case "Lenovo":
-                    cost = 1549;
-                    break;
-            }
-            if (ssd) {
-                cost += 60;
-            }
-            if (printer) {
-                cost += 100;
-            }
-            switch (selectedPeripherals) {
-                case "Cooling Mat":
-                    cost += 33;
-                    break;
-                case "USB C-HUB":
+            RadioButton peripherals = findViewById(peripheralsId);
+            String selectedPeripherals = peripherals.getText().toString();
+
+            double cost = 0;
+            if (selectedComputerType.equals("Desktop")) {
+                if (brand.equals("Dell")) {
+                    cost = 475;
+                } else if (brand.equals("HP")) {
+                    cost = 400;
+                } else if (brand.equals("Lenovo")) {
+                    cost = 450;
+                }
+                if (ssd) {
                     cost += 60;
-                    break;
-                case "Laptop Stand":
-                    cost += 139;
-                    break;
+                }
+                if (printer) {
+                    cost += 100;
+                }
+                if (selectedPeripherals.equals("Webcam")) {
+                    cost += 95;
+                } else if (selectedPeripherals.equals("External Hard Drive")) {
+                    cost += 64;
+                }
+            } else if (selectedComputerType.equals("Laptop")) {
+                switch (brand) {
+                    case "Dell":
+                        cost = 1249;
+                        break;
+                    case "HP":
+                        cost = 1150;
+                        break;
+                    case "Lenovo":
+                        cost = 1549;
+                        break;
+                }
+                if (ssd) {
+                    cost += 60;
+                }
+                if (printer) {
+                    cost += 100;
+                }
+                switch (selectedPeripherals) {
+                    case "Cooling Mat":
+                        cost += 33;
+                        break;
+                    case "USB C-HUB":
+                        cost += 60;
+                        break;
+                    case "Laptop Stand":
+                        cost += 139;
+                        break;
+                }
             }
+
+            double tax = cost * 0.13;
+            double totalCost = cost + tax;
+            String totalPrice = String.format(Locale.CANADA, "%.2f", totalCost);
+
+            String strAdditional = "";
+            if (ssd && printer) {
+                strAdditional = "SSD & Printer";
+            } else if (ssd) {
+                strAdditional = "SSD";
+            } else if (printer) {
+                strAdditional = "Printer";
+            } else {
+                strAdditional = "N/A";
+            }
+
+            String formatInvoiceString = "Customer Name: " + name + "\n" + "Province: " + province + "\n" + "Computer: " + selectedComputerType + "\n" + "Brand: " + brand + "\n" + "Additional: " + strAdditional + "\n" + "Added Peripherals: " + selectedPeripherals + "\n" + "Cost: $" + totalPrice;
+
+            binding.tvInvoice.setText(formatInvoiceString);
+            return true;
+        } catch (Exception e) {
+            return false;
         }
 
-        double tax = cost * 0.13;
-        double totalCost = cost + tax;
-        String totalPrice = String.format(Locale.CANADA, "%.2f", totalCost);
-
-        String strBrand = "";
-        if (ssd && printer) {
-            strBrand = "SSD & Printer";
-        } else if (ssd){
-            strBrand = "SSD";
-        } else if (printer) {
-            strBrand = "Printer";
-        }
-
-        String formatInvoiceString = "Customer Name: " + name + "\n" + "Province: " + province + "\n" + "Computer: " + selectedComputerType + "\n" + "Brand: " + brand + "\n" + "Additional: " + strBrand + "\n" + "Added Peripherals: " + selectedComputerType + "\n" + "Cost: $" + totalPrice;
-        binding.tvInvoice.setText(formatInvoiceString);
-        return true;
     }
 
     private void setupComputerTypeRadioGroup() {
@@ -145,6 +168,8 @@ public class MainActivity extends AppCompatActivity {
         binding.rgComputerType.setOnCheckedChangeListener((group, checkedId) -> {
             if (checkedId == R.id.rbDesktop) {
                 selectedComputerType = "Desktop";
+                selectedPeripheral = "";
+                binding.rgPeripherals.clearCheck();
                 binding.rbNone.setVisibility(View.VISIBLE);
                 binding.rbExternalHardDrive.setVisibility(View.VISIBLE);
                 binding.rbWebcam.setVisibility(View.VISIBLE);
@@ -153,6 +178,8 @@ public class MainActivity extends AppCompatActivity {
                 binding.rbUsbCHub.setVisibility(View.GONE);
             } else if (checkedId == R.id.rbLaptop) {
                 selectedComputerType = "Laptop";
+                selectedPeripheral = "";
+                binding.rgPeripherals.clearCheck();
                 binding.rbNone.setVisibility(View.GONE);
                 binding.rbExternalHardDrive.setVisibility(View.GONE);
                 binding.rbWebcam.setVisibility(View.GONE);
@@ -180,7 +207,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
-
+                selectedBrand = "";
             }
         });
     }
